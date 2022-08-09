bff04c872123d9f70476d01f9f9abaa4c1dcc432
cheikd <104331973+cheikd@users.noreply.github.com>
2022-08-09 19:00:26 +0200

    First upload of the project
    First upload of the project

---
+++ public_ip_logger.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+date=$(date +"%d-%m-%Y"" at ""%H:%M:%S")
+
+logfile="LOG-IP.txt" # Log File name
+
+ip=$(curl -s https://api.ipify.org) #Retrieve Public IP info
+
+lastip=$(tail -n 1 $logfile | awk '{print $NF}')
+
+if [ -n "$lastip" ] #var not empty
+    then
+    if [ "$ip" != "$lastip" ]
+        then
+        echo -e "*** NEW IP ADDRESS DETECTED! ***"
+        echo -e "*** $date - Public IP is --> $ip\n"
+    else
+        echo -e "$date - No new IP found: $ip"
+    fi
+else # var empty
+    echo -e "*** Creation of a New Log File: $date ***"
+    echo -e "*** $date - Public IP is --> $ip"
+fi

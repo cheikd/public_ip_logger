05065ca67e9044dca31433926dd70a4a6dfb00ff
cheikd <104331973+cheikd@users.noreply.github.com>
2022-08-10 16:57:07 +0200

    Adding comments
    Adding comments to the script file

--- public_ip_logger.sh
+++ public_ip_logger.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-date=$(date +"%d-%m-%Y"" at ""%H:%M:%S")
+date=$(date +"%d-%m-%Y"" at ""%H:%M:%S") #Retrive current date and time
 
-logfile="LOG-IP.txt" # Log File name
+logfile="LOG-IP.txt" # Log File name. Change this value if you want to use another filename.
 
 ip=$(curl -s https://api.ipify.org) #Retrieve Public IP info
 
-lastip=$(tail -n 1 $logfile | awk '{print $NF}')
+lastip=$(tail -n 1 $logfile | awk '{print $NF}') #This variable read the logfile and stores the last string of the last line.
 
-if [ -n "$lastip" ] #var not empty
+if [ -n "$lastip" ] # if $lastip not empty
     then
-    if [ "$ip" != "$lastip" ]
+    if [ "$ip" != "$lastip" ] # if current public ip is different than public ip in logfile
         then
         echo -e "*** NEW IP ADDRESS DETECTED! ***"
         echo -e "*** $date - Public IP is --> $ip"
     else
         echo -e "$date - No new IP found: $ip"
     fi
-else # var empty
+else # if $lastip empty
     echo -e "*** Creation of a New Log File: $date ***"
     echo -e "*** $date - Public IP is --> $ip"
 fi
